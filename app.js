23ea9ebab884957557f1ea8b7f8bdb84741df37c
atakurt034 <69969985+atakurt034@users.noreply.github.com>
2020-12-25 22:24:58 +0800

    a

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ import path from 'path'
 const __dirname = path.resolve()
 
 const app = express()
-app.use(express.static(__dirname + 'public'))
+app.use(express.static(path.join(__dirname + 'public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 const PORT = 3000
